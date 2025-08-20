ee43aa14c0909f67b2b530c352ba74583896851c
Deon Kouatchou <ddkouatchou@gmail.com>
2025-08-19 23:23:18 -0400

    Expand stats property enum to include prefix descriptors and updated str and bool representations

--- src/backend/utils/enums/stats.py
+++ src/backend/utils/enums/stats.py
@@ -16,7 +16,70 @@ class StatProperty(Enum):
     NO_PROPERTY = 12
 
     def __str__(self):
-        return self.name
+        match self:
+            case StatProperty.HP_ADD:
+                return "HP"
+            case StatProperty.HP_MUL:
+                return "HP%"
+            case StatProperty.ATK_ADD:
+                return "ATK"
+            case StatProperty.ATK_MUL:
+                return "ATK%"
+            case StatProperty.DEF_ADD:
+                return "DEF"
+            case StatProperty.DEF_MUL:
+                return "DEF%"
+            case StatProperty.SPD:
+                return "SPD"
+            case StatProperty.RES:
+                return "RES"
+            case StatProperty.ACC:
+                return "ACC"
+            case StatProperty.CR:
+                return "CR"
+            case StatProperty.CD:
+                return "CD"
+            case StatProperty.NO_PROPERTY:
+                return ""
     
     def __repr__(self):
-        return self.name
+        return str(self)
+    
+    def __bool__(self):
+        return self != StatProperty.NO_PROPERTY
+    
+    @property
+    def is_additive(self) -> bool:
+        """Check if the stat property is additive."""
+        return self in {
+            StatProperty.HP_ADD, StatProperty.ATK_ADD, StatProperty.DEF_ADD,
+            StatProperty.SPD}
+    
+    @property
+    def prefix_descriptor(self) -> str:
+        """Descriptor for the prefix stat property."""
+        match self:
+            case StatProperty.HP_ADD:
+                return "Strong"
+            case StatProperty.HP_MUL:
+                return "Tenacious"
+            case StatProperty.ATK_ADD:
+                return "Ferocious"
+            case StatProperty.ATK_MUL:
+                return "Powerful"
+            case StatProperty.DEF_ADD:
+                return "Sturdy"
+            case StatProperty.DEF_MUL:
+                return "Durable"
+            case StatProperty.SPD:
+                return "Quick"
+            case StatProperty.RES:
+                return "Resistant"
+            case StatProperty.ACC:
+                return "Intricate"
+            case StatProperty.CR:
+                return "Mortal"
+            case StatProperty.CD:
+                return "Cruel"
+            case StatProperty.NO_PROPERTY:
+                return ""
