74fc8f2b03a952243257cb5c305e42f8350323ef
Deon Kouatchou <ddkouatchou@gmail.com>
2025-08-26 00:08:57 -0400

    Convert data input function outputs to rune entities, separate property and value retrieval

--- src/backend/utils/data_inputs/runes.py
+++ src/backend/utils/data_inputs/runes.py
@@ -1,235 +1,268 @@
-from typing import Optional
+import src.backend.utils.events.runes as rune_events
 
 from src.backend.utils.models.enums.runes import RuneSlot, RuneStars, RuneSet
 from src.backend.utils.models.enums.general import Grade
 from src.backend.utils.models.enums.stats import StatProperty
+from src.backend.utils.models.types.entities import *
 
-def get_rune_slot(ask_input=True) -> RuneSlot:
+def get_rune_slot(ask_input=False) -> RuneSlotEntity:
     """Get the rune slot from user input or return a default value.
     Args:
         ask_input (bool): If True, prompt the user for input. If False, return a default value.
     Returns:
-        RuneSlot: The rune slot selected by the user or a default value.
+        RuneSlotEntity: The rune slot selected by the user or a default value.
     Raises:
         ValueError: If the user input is not a valid rune slot."""
     if not ask_input:
-        return RuneSlot.ONE
+        return RuneSlotEntity(RuneSlot.ONE)
     slot_input = int(input("Enter the rune slot (1-6): "))
     match slot_input:
         case 1:
-            return RuneSlot.ONE
+            return RuneSlotEntity(RuneSlot.ONE)
         case 2:
-            return RuneSlot.TWO
+            return RuneSlotEntity(RuneSlot.TWO)
         case 3:
-            return RuneSlot.THREE
+            return RuneSlotEntity(RuneSlot.THREE)
         case 4:
-            return RuneSlot.FOUR
+            return RuneSlotEntity(RuneSlot.FOUR)
         case 5:
-            return RuneSlot.FIVE
+            return RuneSlotEntity(RuneSlot.FIVE)
         case 6:
-            return RuneSlot.SIX
+            return RuneSlotEntity(RuneSlot.SIX)
         case _:
             raise ValueError("Invalid rune slot entered.")
 
-def get_rune_stars(ask_input=True) -> RuneStars:
+def get_rune_stars(ask_input=False) -> RuneStarsEntity:
     """Get the rune star grade from user input or return a default value.
     Args:
         ask_input (bool): If True, prompt the user for input. If False, return a default value.
     Returns:
-        RuneStars: The rune star grade selected by the user or a default value.
+        RuneStarsEntity: The rune star grade selected by the user or a default value.
     Raises:
         ValueError: If the user input is not a valid rune star grade.
     """
     if not ask_input:
-        return RuneStars.ONE
+        return RuneStarsEntity(RuneStars.ONE)
     star_input = int(input("Enter the rune star grade (1-6): "))
     match star_input:
         case 1:
-            return RuneStars.ONE
+            return RuneStarsEntity(RuneStars.ONE)
         case 2:
-            return RuneStars.TWO
+            return RuneStarsEntity(RuneStars.TWO)
         case 3:
-            return RuneStars.THREE
+            return RuneStarsEntity(RuneStars.THREE)
         case 4:
-            return RuneStars.FOUR
+            return RuneStarsEntity(RuneStars.FOUR)
         case 5:
-            return RuneStars.FIVE
+            return RuneStarsEntity(RuneStars.FIVE)
         case 6:
-            return RuneStars.SIX
+            return RuneStarsEntity(RuneStars.SIX)
         case _:
             raise ValueError("Invalid rune slot entered.")
 
-def get_rune_type(ask_input=True) -> RuneSet:
+def get_rune_type(ask_input=False) -> RuneSetEntity:
     """Get the rune type from user input or return a default value.
     Args:
         ask_input (bool): If True, prompt the user for input. If False, return a default value.
     Returns:
-        RuneSet: The rune type selected by the user or a default value.
+        RuneSetEntity: The rune type selected by the user or a default value.
     Raises:
         ValueError: If the user input is not a valid rune type.
     """
     if not ask_input:
-        return RuneSet.Energy
+        return RuneSetEntity(RuneSet.Energy)
     type_input = str(input("Enter the rune type (Violent, Swift, Despair, etc): ")).strip().lower()
     match type_input:
         case "violent":
-            return RuneSet.Violent
+            return RuneSetEntity(RuneSet.Violent)
         case "swift":
-            return RuneSet.Swift
+            return RuneSetEntity(RuneSet.Swift)
         case "despair":
-            return RuneSet.Despair
+            return RuneSetEntity(RuneSet.Despair)
         case "energy":
-            return RuneSet.Energy
+            return RuneSetEntity(RuneSet.Energy)
         case "fatal":
-            return RuneSet.Fatal
+            return RuneSetEntity(RuneSet.Fatal)
         case "blade":
-            return RuneSet.Blade
+            return RuneSetEntity(RuneSet.Blade)
         case "rage":
-            return RuneSet.Rage
+            return RuneSetEntity(RuneSet.Rage)
         case "will":
-            return RuneSet.Will
+            return RuneSetEntity(RuneSet.Will)
         case "nemesis":
-            return RuneSet.Nemesis
+            return RuneSetEntity(RuneSet.Nemesis)
         case "vampire":
-            return RuneSet.Vampire
+            return RuneSetEntity(RuneSet.Vampire)
         case "shield":
-            return RuneSet.Shield
+            return RuneSetEntity(RuneSet.Shield)
         case "revenge":
-            return RuneSet.Revenge
+            return RuneSetEntity(RuneSet.Revenge)
         case "fight":
-            return RuneSet.Fight
+            return RuneSetEntity(RuneSet.Fight)
         case "determination":
-            return RuneSet.Determination
+            return RuneSetEntity(RuneSet.Determination)
         case "enhance":
-            return RuneSet.Enhance
+            return RuneSetEntity(RuneSet.Enhance)
         case "accuracy":
-            return RuneSet.Accuracy
+            return RuneSetEntity(RuneSet.Accuracy)
         case "tolerance":
-            return RuneSet.Tolerance
+            return RuneSetEntity(RuneSet.Tolerance)
         case _:
             raise ValueError("Invalid rune type entered.")
 
-def get_rune_grade(ask_input=True) -> Grade:
+def get_rune_grade(ask_input=False) -> GradeEntity:
     """Get the rune grade from user input or return a default value.
     Args:
         ask_input (bool): If True, prompt the user for input. If False, return a default value.
     Returns:
-        Grade: The rune grade selected by the user or a default value.
+        GradeEntity: The rune grade selected by the user or a default value.
     Raises:
         ValueError: If the user input is not a valid rune grade.
     """
     if not ask_input:
-        return Grade.HERO
+        return GradeEntity(Grade.HERO)
     grade_input = str(input("Enter the rune grade (Normal, Magic, Rare, Hero, Legend): ")).strip().lower()
     match grade_input:
         case "normal":
-            return Grade.NORMAL
+            return GradeEntity(Grade.NORMAL)
         case "magic":
-            return Grade.MAGIC
+            return GradeEntity(Grade.MAGIC)
         case "rare":
-            return Grade.RARE
+            return GradeEntity(Grade.RARE)
         case "hero":
-            return Grade.HERO
+            return GradeEntity(Grade.HERO)
         case "legend":
-            return Grade.LEGEND
+            return GradeEntity(Grade.LEGEND)
         case _:
             raise ValueError("Invalid rune grade entered.")
 
-def get_main_stat(slot: RuneSlot, ask_input=True) -> Optional[StatProperty]:
-    """Get the main stat property for a rune slot from user input or return a default value or None.
+def get_rune_stage(grade: Grade) -> ValueEntity:
+    """Get stage of rune based on its grade. Currently not customizable.
+    Args:
+        grade (Grade): rune grade
+    Returns:
+        ValueEntity: rune stage (0-12)
+    Raises:
+        ValueError: invalid rune grade
+    """
+    match grade:
+        case Grade.NORMAL:
+            return ValueEntity(0)
+        case Grade.MAGIC:
+            return ValueEntity(3)
+        case Grade.RARE:
+            return ValueEntity(6)
+        case Grade.HERO:
+            return ValueEntity(9)
+        case Grade.LEGEND:
+            return ValueEntity(12)
+
+def get_main_stat_property(slot: RuneSlot, ask_input=False) -> PropertyEntity:
+    """Get the main stat property for a rune slot from user input or return a random property.
     Args:
         slot (RuneSlot): The rune slot for which to get the main stat property.
         ask_input (bool): If True, prompt the user for input. If False, return None.
     Returns:
-        Optional[StatProperty]: The main stat property for the rune slot or None if not applicable.
+        PropertyEntity: The main stat property for the rune slot.
     Raises:
         ValueError: If the user input is not a valid main stat property.
     """
-    if slot in [RuneSlot.ONE, RuneSlot.THREE, RuneSlot.FIVE]:
-        return None
+    event = rune_events.get_rune_main_stat_property_event(slot)
+    if not ask_input:
+        return PropertyEntity(event.sample(), event)
     else:
-        if not ask_input:
-            return StatProperty.HP_MUL
         answer = str(input("Enter a main stat property? (y/n): ")).strip().lower()
         if answer == 'y':
             stat_input = str(input("Enter the property (ATK+/HP+/DEF+, ATK%/HP%/DEF%, SPD, CR, CD, RES, ACC): ")).strip().lower()
             match stat_input:
                 case "atk+":
-                    return StatProperty.ATK_ADD
+                    return PropertyEntity(StatProperty.ATK_ADD, event)
                 case "hp+":
-                    return StatProperty.HP_ADD
+                    return PropertyEntity(StatProperty.HP_ADD, event)
                 case "def+":
-                    return StatProperty.DEF_ADD
+                    return PropertyEntity(StatProperty.DEF_ADD, event)
                 case "atk%":
-                    return StatProperty.ATK_MUL
+                    return PropertyEntity(StatProperty.ATK_MUL, event)
                 case "hp%":
-                    return StatProperty.HP_MUL
+                    return PropertyEntity(StatProperty.HP_MUL, event)
                 case "def%":
-                    return StatProperty.DEF_MUL
+                    return PropertyEntity(StatProperty.DEF_MUL, event)
                 case "spd":
-                    return StatProperty.SPD
+                    return PropertyEntity(StatProperty.SPD, event)
                 case "cr":
-                    return StatProperty.CR
+                    return PropertyEntity(StatProperty.CR, event)
                 case "cd":
-                    return StatProperty.CD
+                    return PropertyEntity(StatProperty.CD, event)
                 case "res":
-                    return StatProperty.RES
+                    return PropertyEntity(StatProperty.RES, event)
                 case "acc":
-                    return StatProperty.ACC
+                    return PropertyEntity(StatProperty.ACC, event)
                 case _:
                     raise ValueError("Invalid main stat property entered.")
         else:
-            return None
+            return PropertyEntity(event.sample(), event)
 
-def get_prefix_stat(ask_input=True) -> tuple[Optional[StatProperty], Optional[int]]:
-    """Get the prefix stat property and value from user input or return a default value.
+def get_prefix_property(slot: RuneSlot, main_property: StatProperty, ask_input=False) -> PropertyEntity:
+    """Get the prefix stat property and value from user input or return a random value.
     Args:
-        ask_input (bool): If True, prompt the user for input. If False, return a default value.
+        ask_input (bool): If True, prompt the user for input. If False, return a random value.
     Returns:
-        tuple[Optional[StatProperty], Optional[int]]: A tuple containing the prefix stat property and value.
+        PropertyEntity: The prefix stat property
     Raises:
         ValueError: If the user input is not a valid prefix stat property.
     """
+    event = rune_events.get_rune_prefix_stat_property_event(slot, main_property)
     if not ask_input:
-        return StatProperty.ACC, 7
+        return PropertyEntity(event.sample(), event)
     answer = str(input("Enter a prefix stat property? (y/n): ")).strip().lower()
     if answer == 'y':
         prop_input = str(input("Enter the prefix stat property (ATK+/HP+/DEF+, ATK%/HP%/DEF%, SPD, CR, CD, RES, ACC): ")).strip().lower()
         match prop_input:
             case "atk+":
-                prop_input = StatProperty.ATK_ADD
+                return PropertyEntity(StatProperty.ATK_ADD, event)
             case "hp+":
-                prop_input = StatProperty.HP_ADD
+                return PropertyEntity(StatProperty.HP_ADD, event)
             case "def+":
-                prop_input = StatProperty.DEF_ADD
+                return PropertyEntity(StatProperty.DEF_ADD, event)
             case "atk%":
-                prop_input = StatProperty.ATK_MUL
+                return PropertyEntity(StatProperty.ATK_MUL, event)
             case "hp%":
-                prop_input = StatProperty.HP_MUL
+                return PropertyEntity(StatProperty.HP_MUL, event)
             case "def%":
-                prop_input = StatProperty.DEF_MUL
+                return PropertyEntity(StatProperty.DEF_MUL, event)
             case "spd":
-                prop_input = StatProperty.SPD
+                return PropertyEntity(StatProperty.SPD, event)
             case "cr":
-                prop_input = StatProperty.CR
+                return PropertyEntity(StatProperty.CR, event)
             case "cd":
-                prop_input = StatProperty.CD
+                return PropertyEntity(StatProperty.CD, event)
             case "res":
-                prop_input = StatProperty.RES
+                return PropertyEntity(StatProperty.RES, event)
             case "acc":
-                prop_input = StatProperty.ACC
+                return PropertyEntity(StatProperty.ACC, event)
             case _:
                 raise ValueError("Invalid stat property entered.")
-        
-        answer = str(input("Enter a prefix stat value? (y/n): ")).strip().lower()
-        if answer == 'y':
-            value_input = int(input("Enter the prefix stat value: "))
-        else:
-            value_input = None
-        return prop_input, value_input
     else:
-        prop_input, value_input = (None, None)
-    return prop_input, value_input
+        return PropertyEntity(event.sample(), event)
+
+def get_prefix_stat_value(stars: RuneStars, prefix_stat_property: StatProperty, ask_input=False) -> ValueEntity:
+    """Get the prefix stat value from user input or return a random value.
+    Args:
+        ask_input (bool): If True, prompt the user for input. If False, return a default value.
+    Returns:
+        ValueEntity: The prefix stat value.
+    Raises:
+        ValueError: If the user input is not a valid prefix stat property.
+    """
+    value_event = rune_events.get_rune_prefix_stat_value_event(stars, prefix_stat_property)
+    if not ask_input:
+        return ValueEntity(value_event.sample(), value_event)
+    answer = str(input("Enter a prefix stat value? (y/n): ")).strip().lower()
+    if answer == 'y':
+        value = int(input("Enter the prefix stat value: "))
+        return ValueEntity(value, value_event)
+    else:
+        return ValueEntity(value_event.sample(), value_event)
 
 def get_sub_stat(count: int):
     """Get the sub-stat property and value from user input.
@@ -270,7 +303,7 @@ def get_sub_stat(count: int):
     value_input = int(input("Enter the sub-stat value: "))
     return prop_input, value_input
 
-def get_sub_stats(grade: Grade, ask_input=True) -> tuple[list[StatProperty], list[int]]:
+def get_sub_stats(grade: Grade, ask_input=False) -> tuple[list[StatProperty], list[int]]:
     """Get the sub-stat properties and values based on the rune grade.
     Args:
         grade (Grade): The rune grade for which to get the sub-stat properties and values.
