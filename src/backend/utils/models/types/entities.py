b3efa4464def0d8dbe31e4522149d48c943d1327
Deon Kouatchou <ddkouatchou@gmail.com>
2025-08-26 00:05:43 -0400

    Add arg checker to entity class

--- src/backend/utils/models/types/entities.py
+++ src/backend/utils/models/types/entities.py
@@ -2,12 +2,20 @@ from typing import TypeVar, Hashable, Optional
 
 from src.backend.utils.models.types.events import Event
 from src.backend.utils.models.enums.stats import StatProperty
+from src.backend.utils.models.enums.runes import RuneSlot, RuneSet, RuneStars
+from src.backend.utils.models.enums.general import Grade
 
 T = TypeVar('T', bound=Hashable)
 
 class Entity[T]:
     """Class representing an entity which contains an value and its associated event.
-    
+
+    The value is an outcome and the associated event is CONDITIONAL on outcomes of some other event.
+    i.e. For an Entity object with an outcome `a` and event `E`, then
+
+    - E = RandomVar(A | B=b, C=c)
+    - a \\in E
+
     Attributes:
         value (T): The value of the entity, which must be hashable.
         event (Optional[Event[T]]): The event associated with the entity, if any.
@@ -16,10 +24,24 @@ class Entity[T]:
     """
 
     def __init__(self, value: T, event: Optional[Event[T]] = None):
+        self._check_args(value, event)
         self._value = value
         self._event = event
         self._is_fixed = event is None
+        self._check_args(value, event)
     
+    def _check_args(self, value, event) -> None:
+        """Checks for the following constraints on object initialization:
+        1. The value is a possible outcome of the event
+
+        Args:
+            value (T): Value provided in initialization
+            event (Event[T]): Event provided in initialization
+        Raises:
+            AttributeError: does not adhere to one of the constraints"""
+        if value not in event:
+            raise AttributeError(f"Value {value} is not a possible outcome of event")
+
     @property
     def value(self) -> T:
         """Returns the value of the entity."""
@@ -37,5 +59,10 @@ class Entity[T]:
             return 1.0
         return self.event[self.value]
 
+
 ValueEntity = Entity[int]
 PropertyEntity = Entity[StatProperty]
+RuneStarsEntity = Entity[RuneStars]
+RuneSlotEntity = Entity[RuneSlot]
+RuneSetEntity = Entity[RuneSet]
+GradeEntity = Entity[Grade]
