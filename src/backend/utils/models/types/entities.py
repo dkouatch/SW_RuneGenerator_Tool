7cb4ca48066834b4552245855d9ce5955a441ab4
Deon Kouatchou <ddkouatchou@gmail.com>
2025-08-28 14:37:30 -0400

    Add fixed event support to entity class

--- src/backend/utils/models/types/entities.py
+++ src/backend/utils/models/types/entities.py
@@ -23,14 +23,18 @@ class Entity[T]:
                          or ignores the event's probabilities.
     """
 
-    def __init__(self, value: T, event: Optional[Event[T]] = None):
-        self._check_args(value, event)
+    def __init__(self,
+                 value: T,
+                 event: Optional[Event[T]] = None,
+                 fixed_event: Optional[Event[T]] = None) -> None:
+        self._check_args(value, event, fixed_event)
         self._value = value
         self._event = event
+        self._fixed_event = fixed_event
         self._is_fixed = event is None
-        self._check_args(value, event)
+        self._check_args(value, event, fixed_event)
     
-    def _check_args(self, value, event) -> None:
+    def _check_args(self, value, event, fixed_event) -> None:
         """Checks for the following constraints on object initialization:
         1. The value is a possible outcome of the event
 
@@ -39,8 +43,10 @@ class Entity[T]:
             event (Event[T]): Event provided in initialization
         Raises:
             AttributeError: does not adhere to one of the constraints"""
-        if value not in event:
+        if event is not None and value not in event:
             raise AttributeError(f"Value {value} is not a possible outcome of event")
+        elif fixed_event is not None and value not in fixed_event:
+            raise AttributeError(f"Value {value} is not a possible outcome of fixed_event")
 
     @property
     def value(self) -> T:
@@ -53,15 +59,32 @@ class Entity[T]:
         return self._event
     
     @property
+    def fixed_event(self) -> Optional[Event[T]]:
+        """Returns the fixed event associated with the entity."""
+        return self._fixed_event
+    
+    def fix(self) -> None:
+        """Fixes the entity, making it consider its fixed event's probabilities."""
+        self._is_fixed = True
+    
+    def unfix(self) -> None:
+        """Unfixes the entity, making it consider its event's probabilities."""
+        self._is_fixed = False
+    
+    @property
     def probability(self) -> float:
         """Returns the probability of the value."""
         if self.is_fixed:
-            return 1.0
+            if self.fixed_event is None:
+                return 1.0
+            else:
+                return self.fixed_event[self.value]
         return self.event[self.value]
 
 
 ValueEntity = Entity[int]
 PropertyEntity = Entity[StatProperty]
+SubPropertyEntity = Entity[tuple[StatProperty, ...]]
 RuneStarsEntity = Entity[RuneStars]
 RuneSlotEntity = Entity[RuneSlot]
 RuneSetEntity = Entity[RuneSet]
