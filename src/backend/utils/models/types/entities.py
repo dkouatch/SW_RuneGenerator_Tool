d84f0cf787d1a4a92dd0f027f1feb81535003f58
Deon Kouatchou <ddkouatchou@gmail.com>
2026-09-09 17:35:29 -0400

    Complete redo of Entity class:
    * Rename attributes `event` and `fixed_event` to `unconditional_event` and `conditional_event`, respectively, for more clarity
    * Rename `is_fixed`, `fix()`, and `unfix()` to `is_conditioned`, `condition()`, and `uncondition()`, respecitvely.
    * Add `sorted()`, `to_counter()`, `reduce()`, and `map()` functions to class

--- src/backend/utils/models/types/entities.py
+++ src/backend/utils/models/types/entities.py
@@ -1,92 +1,203 @@
-from typing import TypeVar, Hashable, Optional
+from __future__ import annotations
 
-from src.backend.utils.models.types.events import Event
+from functools import reduce
+from typing import TypeVar, Hashable, Generic, Callable, overload, cast
+
+from src.backend.utils.models.types.events import Event, SortableHashable
 from src.backend.utils.models.enums.stats import StatProperty
 from src.backend.utils.models.enums.runes import RuneSlot, RuneSet, RuneStars
-from src.backend.utils.models.enums.general import Grade, Upgrade
+from src.backend.utils.models.enums.general import Grade, Upgrade, HashableCounter
 
 T = TypeVar('T', bound=Hashable)
 
-class Entity[T]:
+# TODO: Dungeon class for representing drops where we template full rune event outcomes
+
+class Entity(Generic[T]):
     """Class representing an entity which contains an value and its associated event.
 
     The value is an outcome and the associated event is CONDITIONAL on outcomes of some other event.
-    i.e. For an Entity object with an outcome `a` and event `E` and some fixed event `F`, then
+    i.e. For an Entity object with an outcome `a` and unconditional event `E` and some conditional
+    event `F`, then
 
-    - E = RandomVar(A | B=b, C=c)
-    - F = RandomVar(A | B=b, C=c, D=d)
+    - E = RandomVar(A)
+    - F = RandomVar(A | B=b)
     - a \\in E and a \\in F
     - F is a subset of E
+
+    Almost all entities are of the first form where you have a \\in E for a fixed event E.
+    Only sub-property events may have the additional thing. Now is this necessary? Idk.
 
     Supports creating entities over None
 
     Attributes:
-        value (Optional[T]): The value of the entity, which must be hashable.
-        event (Optional[Event[T]]): The event associated with the entity, independent of some fixed choices within the value.
-        fixed_event (Optional[Event[T]]): The event associated with the entity, dependent of some fixed choices within the value.
+        outcome (T): The value of the entity, an outcome, which must be hashable.
+        unconditional_event (Event[T]): The event associated with the entity, independent of some fixed choices within the value.
+        conditional_event (Event[T] | None): The event associated with the entity, dependent of some fixed choices within the value.
         probability (float): The relevant probability of the entity according to whether it is fixed
-        is_fixed (bool): State of the entity
+        is_conditioned (bool): State of the entity
     """
 
     def __init__(self,
-                 value: Optional[T],
-                 event: Optional[Event[T]] = None,
-                 fixed_event: Optional[Event[T]] = None) -> None:
-        self._check_args(value, event, fixed_event)
-        self._value = value
-        self._event = event
-        self._fixed_event = fixed_event
-        self._is_fixed = event is None
-        self._check_args(value, event, fixed_event)
-        print(f"Created Entity over type {type(value)}")
+                 outcome: T,
+                 unconditional_event: Event[T],
+                 conditional_event: Event[T] | None = None) -> None:
+        self._check_args(outcome, unconditional_event, conditional_event)
+        self._outcome = outcome
+        self._unconditional_event = unconditional_event
+        self._conditional_event = conditional_event
+        self._is_conditioned = conditional_event is not None  # By default, True if conditional event provided
+        print(f"Created Entity over type {type(outcome)}")
     
-    def _check_args(self, value, event, fixed_event) -> None:
-        """Checks for the following constraints on object initialization:
-        1. The value is a possible outcome of the event
+    def _check_args(
+        self,
+        outcome: T,
+        unconditional_event: Event[T],
+        conditional_event: Event[T] | None) -> None:
+        """Enforces the following constraints
+        
+        1. The outcome is possible in the unconditional event.
+        2. If a conditional event exists, the outcome is possible.
+        3. If a conditional event exists, the set of outcomes in the conditional event
+           is a subset of outcomes in the unconditional event.
 
         Args:
-            value (Optional[T]): Value provided in initialization
-            event (Event[T]): Event provided in initialization
+            outcome (Optional[T]): Outcome value provided in initialization
+            unconditional_event (Event[T]): Unconditional event provided in initialization
+            conditional_event (Event[T] | None): Conditional event provided in intiialization
         Raises:
-            AttributeError: does not adhere to one of the constraints"""
-        if event is not None and value not in event:
-            raise AttributeError(f"Value {value} is not a possible outcome of event")
-        elif fixed_event is not None and value not in fixed_event:
-            raise AttributeError(f"Value {value} is not a possible outcome of fixed_event")
+            AttributeError: does not adhere to the constraints
+        """
+        if outcome not in unconditional_event:
+            raise AttributeError(f"Value {outcome} is not a possible outcome of the unconditional event.")
+        if conditional_event is not None:
+            if outcome not in conditional_event:
+                raise AttributeError(f"Value {outcome} is not a possible outcome of the conditional event.")
+            elif not conditional_event.outcomes.issubset(unconditional_event.outcomes):
+                raise AttributeError("Set of outcomes in conditional event is not a subset of the set of outcomes in the unconditional event.")
 
     @property
-    def value(self) -> T:
+    def outcome(self) -> T:
         """Returns the value of the entity."""
-        return self._value
+        return self._outcome
     
     @property
-    def event(self) -> Optional[Event[T]]:
-        """Returns the event associated with the entity."""
-        return self._event
+    def unconditional_event(self) -> Event[T]:
+        """Returns the unconditional event associated with the entity."""
+        return self._unconditional_event
     
     @property
-    def fixed_event(self) -> Optional[Event[T]]:
-        """Returns the fixed event associated with the entity."""
-        return self._fixed_event
+    def conditional_event(self) -> Event[T] | None:
+        """Returns the conditional event associated with the entity."""
+        return self._conditional_event
     
-    def fix(self) -> None:
-        """Fixes the entity, making it consider its fixed event's probabilities."""
-        self._is_fixed = True
+    def condition(self) -> None:
+        """Conditions the entity on the conditional event's probabilities, or to 1.0 if it does not exist."""
+        self._is_conditioned = True
     
-    def unfix(self) -> None:
-        """Unfixes the entity, making it consider its event's probabilities."""
-        self._is_fixed = False
+    def uncondition(self) -> None:
+        """Unconditions the entity to switch to the unconditional event's probabilities."""
+        self._is_conditioned = False
     
     @property
     def probability(self) -> float:
-        """Returns the probability of the value."""
-        if self._is_fixed:
-            if self.fixed_event is None:
+        """Returns the probability of the outcome."""
+        if self._is_conditioned:
+            if self.conditional_event is None:  # If no conditional event exists, returns probability of 1.0
                 return 1.0
             else:
-                return self.fixed_event[self.value]
+                return self.conditional_event[self.outcome]
         else:
-            return self.event[self.value]
+            return self.unconditional_event[self.outcome]
+
+    def sorted[V: SortableHashable](self: Entity[tuple[V, ...]]) -> Entity[tuple[V, ...]]:
+        """Creates new entity where event outcomes that are previously unsorted tuples become sorted
+
+        NOTE: Recall that sorted events should not be manipulated in the same manner as normal events
+
+        Returns:
+            Entity[tuple[SortableHashable, ...]]: New entity with sorted events and outcome
+        """
+        new_outcome = tuple(sorted(self.outcome))
+        new_unconditional_event = self.unconditional_event.sorted()
+        new_conditional_event = None if self.conditional_event is None else self.conditional_event.sorted()
+        return Entity[tuple[V, ...]](new_outcome, new_unconditional_event, new_conditional_event) 
+    
+    def to_counter[V: Hashable](self: Entity[tuple[V, ...]]) -> Entity[HashableCounter[V]]:
+        """Creates new entity where event outcomes that are tuples over type V
+        are converted into counter objects over type V.
+
+        E.g., `('apple', 'orange', 'banana', 'orange', 'apple') -> {'apple': 2, 'banana': 1, 'orange': 2}`
+
+        Returns:
+            Entity[HashableCounter[V]]: New entity of counter events and outcome instead of tuples
+        """
+        new_outcome = HashableCounter[V](self.outcome)
+        new_unconditional_event = self.unconditional_event.to_counter()
+        new_conditional_event = None if self.conditional_event is None else self.conditional_event.to_counter()
+        return Entity[HashableCounter[V]](new_outcome, new_unconditional_event, new_conditional_event) 
+    
+    @overload
+    def reduce[V: Hashable](
+        self: Entity[tuple[V, ...]],
+        op: Callable[[V, V], V]) -> Entity[V]: ...
+
+    @overload
+    def reduce[U: Hashable, V: Hashable](
+        self: Entity[tuple[U, ...]],
+        op: Callable[[V, U], V],
+        initial: V) -> Entity[V]: ...
+    
+    def reduce[V: Hashable](
+            self: Entity[tuple[Hashable, ...]],
+            op,
+            initial: V | None = None) -> Entity[V]:
+        """Tries to reduce the entity if event outcomes are tuples using a folding function.
+        Effectively folds sequences of smaller events into a meaningful, aggregate representation.
+
+        The folding operation is left-associative:
+        
+        `f ( f ( ... f(z, 1) ...), n-1), n)`
+
+        Args:
+            op (Callable[[V, V], V] | Callable[[V, U], V]): Folding function to apply to the outcome and events
+            initial (V | None): Initial folding value
+        Returns:
+            Entity[V]: A new entity with reduced events and outcome
+        """
+        new_outcome = (
+            reduce(op, self.outcome) if initial is None 
+            else reduce(op, self.outcome, initial))
+        new_outcome = cast(V, new_outcome)
+
+        new_unconditional_event = (
+            self.unconditional_event.reduce(op) if initial is None
+            else self.unconditional_event.reduce(op, initial))
+        new_unconditional_event = cast(Event[V], new_unconditional_event)
+
+        if self.conditional_event is None:
+            new_conditional_event = None
+        else:
+            new_conditional_event = (
+                self.conditional_event.reduce(op) if initial is None
+                else self.conditional_event.reduce(op, initial))
+            new_conditional_event = cast(Event[V], new_conditional_event)
+
+        return Entity[V](new_outcome, new_unconditional_event, new_conditional_event)
+    
+    def map[V: Hashable](
+            self,
+            func: Callable[[T], V]) -> Entity[V]:
+        """Maps the outcomes of the event to a new type using a mapping function
+
+        Args:
+            func (Callable[[T], V]): Function to apply to each outcome
+        Returns:
+            Event[V]: New event with mapped outcomes
+        """
+        new_outcome = func(self.outcome)
+        new_unconditional_event = self.unconditional_event.map(func)
+        new_conditional_event = None if self.conditional_event is None else self.conditional_event.map(func)
+        return Entity[V](new_outcome, new_unconditional_event, new_conditional_event) 
 
 
 ValueEntity = Entity[int]
@@ -96,4 +207,4 @@ RuneStarsEntity = Entity[RuneStars]
 RuneSlotEntity = Entity[RuneSlot]
 RuneSetEntity = Entity[RuneSet]
 GradeEntity = Entity[Grade]
-UpgradeEntity = Entity[Upgrade]
+UpgradeEntity = Entity[tuple[Upgrade, ...]]
